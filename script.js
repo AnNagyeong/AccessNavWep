572a79dd67943a1696e7f5ddedbe37ac4dfcd66d
Mint <hun111077@gmail.com>
2026-04-16 11:52:01 +0900

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const container = document.getElementById('map');
-
-const options = {
-  center: new kakao.maps.LatLng(37.56184, 127.03811),
-  level: 3
-};
-
-const map = new kakao.maps.Map(container, options);
